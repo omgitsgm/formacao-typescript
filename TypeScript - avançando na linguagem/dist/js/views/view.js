9c348741347a698726244911495a5d6b4493e027
Gabriel Mendes <luizgabrielpm@gmail.com>
2023-01-04 18:48:24 -0300

    TypeScript - avançando na linguagem
    - 80% concluído

---
+++ TypeScript - avançando na linguagem/dist/js/views/view.js
@@ -0,0 +1,17 @@
+export class View {
+    // escapar?: boolean -> é uma variável opcional do construtor
+    constructor(seletor, escapar) {
+        this.escapar = false;
+        this.elemento = document.querySelector(seletor);
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+    update(model) {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.elemento.innerHTML = template;
+    }
+}
